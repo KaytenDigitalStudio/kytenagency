abd4472c8851871a1c65e17c13fcd48038b36c8b
four44488@gmail.com <four44488@gmail.com>
2022-03-05 14:56:03 +0100

    scroll bugs

--- src/components/ScrollBar/ScrollBar.jsx
+++ src/components/ScrollBar/ScrollBar.jsx
@@ -1,29 +1,31 @@
+
 import React, {
   useEffect
 } from 'react';
 import classes from '../../styles/ScrollBar.module.scss';
 
 function ScrollBar() {
-  // const sections = useRef();
   const sections = document.querySelectorAll('section');
   const navigation = document.getElementById('navigation');
   const windowHeight = window.innerHeight;
 
 
 
-  function resetSelection() {
-    for (var i = 0; i < navigation.children.length; i++) {
-      navigation.children[i].style.background = "none"
-    }
-  }
+ 
   const handleScroll = () => {
     const scrollTop = window.pageYOffset;
     sections.forEach((section, i) => {
       if (section.offsetTop < scrollTop + windowHeight / 2 && scrollTop < section.offsetTop + windowHeight / 2) {
         resetSelection()
-        navigation.children[i].style.background = "#fff"
+        navigation.children[i].classList.add(classes.selected)
       }
     })
+  }
+  
+  function resetSelection() {
+    for (var i = 0; i < navigation.children.length; i++) {
+      navigation.children[i].classList.remove(classes.selected)
+    }
   }
   // navigation.querySelectorAll('li').forEach(function(item, i) {
   //   item.addEventListener('click', function() {
@@ -33,17 +35,18 @@ function ScrollBar() {
   //       behavior: 'smooth'
   //     });
   //   });
-  // });
-
+  // // });
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
-
     return () => {
+
       window.removeEventListener('scroll', handleScroll);
     };
-  }, );
+  });
+  
 
+ 
 
   return ( 
     < ul className={ classes.navigation } id='navigation'>
@@ -52,7 +55,7 @@ function ScrollBar() {
         <li></li>
         <li></li>
         <li></li>
-    </ul>
-                )
-                }
+   </ul>
+  )
+  }
 export default ScrollBar
